9ffda6a102394a0cd1d60c448889e29288143b2f
VenenAlex <venenalex2004@gmail.com>
2025-04-10 10:57:17 +0530

    Added new lines

--- InitialPhase/sample.py
+++ InitialPhase/sample.py
@@ -1,3 +1,10 @@
+import random
 name = input("Enter your name : ")
 print(f"Hello {name}, Have a nice day!!")
 print(f"The length of your name is : {len(name)}")
+random_num = random.randint(1,10)
+guess = input("Guess a number between 1 - 10 : ")
+if (guess==random_num):
+    print(f"You guessed the correct number!!")
+else:
+    print(f"Wrong!!, The correct number was {random_num}")
